8cb02cf20266c5a9f692f133288a0b3193be9607
aasthaghule2004 <aasthaghule4@gmail.com>
2025-05-18 20:50:57 +0530

    initial commit

---
+++ roboSpeak.py
@@ -0,0 +1,11 @@
+import pyttsx3
+
+if __name__ == '__main__':
+    print("Welcome to RoboSpeaker")
+    while True:
+        x = input("Enter what you want me to speak: ")
+        if x == "q":
+            break
+        engine = pyttsx3.init()
+        engine.say(x)
+        engine.runAndWait()
